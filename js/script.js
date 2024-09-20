8d1931ceaa692d819a0bda383c2e367507adb89e
khalid Reda <khalidaziz2198@gmail.com>
2024-09-20 23:23:53 +0300

    Start Jonas js learning journey

--- js/script.js
+++ js/script.js
@@ -2,3 +2,18 @@ let js = "amazing";
 if (js === "amazing") alert("javascript if fun!");
 
 console.log(40 + 8 + 23 - 10);
+
+let first = "khalid";
+let firstName = "hello";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myName = "JM";
+console.log();
+let myFirstJob = "coder";
+let myCurrentJob = "teacher";
+console.log(myFirstJob);
