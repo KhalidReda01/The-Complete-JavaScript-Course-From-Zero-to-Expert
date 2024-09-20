2a5d1a6da7322f0669035cab755cd670e46e3850
khalid Reda <khalidaziz2198@gmail.com>
2024-09-21 00:06:10 +0300

    Lecture 7 Data Types

--- js/script.js
+++ js/script.js
@@ -1,19 +1,37 @@
-let js = "amazing";
-if (js === "amazing") alert("javascript if fun!");
+// let js = "amazing";
+// if (js === "amazing") alert("javascript if fun!");
 
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 
-let first = "khalid";
-let firstName = "hello";
+// let first = "khalid";
+// let firstName = "hello";
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-let myName = "JM";
-console.log();
-let myFirstJob = "coder";
-let myCurrentJob = "teacher";
-console.log(myFirstJob);
+// let myName = "JM";
+// console.log();
+// let myFirstJob = "coder";
+// let myCurrentJob = "teacher";
+// console.log(myFirstJob);
+true;
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+console.log(typeof true);
+console.log(typeof "khalid");
+
+javascriptIsFun = "Yes";
+console.log(typeof javascriptIsFun);
+let year;
+
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+
+console.log(year);
+console.log(typeof year);
