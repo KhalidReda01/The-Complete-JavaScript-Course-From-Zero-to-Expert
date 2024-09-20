6115aaac2e579413378590fa886eefa44edf0742
khalid Reda <khalidaziz2198@gmail.com>
2024-09-21 02:19:17 +0300

    Lecture 11 coding Challenge done without repetition and reinforcing your knowlege It's impossible to really learn how to code

--- js/script.js
+++ js/script.js
@@ -94,14 +94,14 @@ m tall.
 
 // let mass;
 // let height;
-const markMass = 78;
-const markHeight = 1.69;
-const johnMass = 92;
-const johnHeight = 1.95;
-// const markMass = 95;
-// const markHeight = 1.88;
-// const johnMass = 85;
-// const johnHeight = 1.76;
+// const markMass = 78;
+// const markHeight = 1.69;
+// const johnMass = 92;
+// const johnHeight = 1.95;
+const markMass = 95;
+const markHeight = 1.88;
+const johnMass = 85;
+const johnHeight = 1.76;
 
 const CalcMarkBMI = markMass / markHeight ** 2;
 const CalcJohnBMI = johnMass / johnHeight ** 2;
