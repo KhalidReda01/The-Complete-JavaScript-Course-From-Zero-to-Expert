b9d1fc64acaccaca1c8d7e6c1ae3627f741d9529
khalid Reda <khalidaziz2198@gmail.com>
2024-09-22 00:04:28 +0300

    Lecture 12 strings and template literls assignment

--- js/assignment.js
+++ js/assignment.js
@@ -1,17 +1,17 @@
-// Lecture Values and Variables
-const country = "Egypt";
-const continent = "Africa";
-let population = "120 milion";
-let yourCountrySplitted = true;
-let countrySecond = "Finland";
-let populationSecond = "6 million";
-let countPopulation = population / 2;
-console.log(countPopulation++);
-console.log(countPopulation);
-console.log();
-// console.log(country);
-// console.log(continent);
-// console.log(population);
+// // Lecture Values and Variables
+// const country = "Egypt";
+// const continent = "Africa";
+// let population = "120 milion";
+// let yourCountrySplitted = true;
+// let countrySecond = "Finland";
+// let populationSecond = "6 million";
+// let countPopulation = population / 2;
+// console.log(countPopulation++);
+// console.log(countPopulation);
+// console.log();
+// // console.log(country);
+// // console.log(continent);
+// // console.log(population);
 
 //Lecture Data Types
 const isIsland = false;
@@ -21,6 +21,7 @@ lang = "Arabic";
 // console.log(typeof population);
 // console.log();
 console.log(population < populationSecond);
-let description =
-  country + " is in " + continent + " and its " + population + " speak " + lang;
+// let description = country + " is in " + continent + " and its " + population + " speak " + lang;
+let description = `${country}  is in ${continent}   and its ${population}  speak ${lang} ;`;
+
 console.log(description);
--- js/script.js
+++ js/script.js
@@ -98,13 +98,13 @@ m tall.
 // const markHeight = 1.69;
 // const johnMass = 92;
 // const johnHeight = 1.95;
-const markMass = 95;
-const markHeight = 1.88;
-const johnMass = 85;
-const johnHeight = 1.76;
+// const markMass = 95;
+// const markHeight = 1.88;
+// const johnMass = 85;
+// const johnHeight = 1.76;
 
-const CalcMarkBMI = markMass / markHeight ** 2;
-const CalcJohnBMI = johnMass / johnHeight ** 2;
-const markHigherBMI = CalcMarkBMI > CalcJohnBMI;
-console.log(CalcMarkBMI, CalcJohnBMI);
-console.log(markHigherBMI);
+// const CalcMarkBMI = markMass / markHeight ** 2;
+// const CalcJohnBMI = johnMass / johnHeight ** 2;
+// const markHigherBMI = CalcMarkBMI > CalcJohnBMI;
+// console.log(CalcMarkBMI, CalcJohnBMI);
+// console.log(markHigherBMI);
