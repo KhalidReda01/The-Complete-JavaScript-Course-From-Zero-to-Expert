6dc42457d449bace53c36d192584a12309d1212d
khalid Reda <khalidaziz2198@gmail.com>
2024-09-22 17:11:30 +0300

    Lecture 15 Type converstion and coercion

--- js/script.js
+++ js/script.js
@@ -94,10 +94,10 @@ m tall.
 
 // let mass;
 // let height;
-// const markMass = 78;
-// const markHeight = 1.69;
-// const johnMass = 92;
-// const johnHeight = 1.95;
+const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 92;
+const johnHeight = 1.95;
 
 // const age = 15;
 // const isOldEnough = age >= 18;
@@ -124,21 +124,28 @@ m tall.
 // 2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
 // BMI (28.3) is higher than John's (23.9)!"
 // Hint: Use an if/else statement
-const markMass = 95;
-const markHeight = 1.88;
-const johnMass = 85;
-const johnHeight = 1.76;
+// const markMass = 95;
+// const markHeight = 1.88;
+// const johnMass = 85;
+// const johnHeight = 1.76;
 
-const CalcMarkBMI = markMass / markHeight ** 2;
-const CalcJohnBMI = johnMass / johnHeight ** 2;
-const markHigherBMI = CalcMarkBMI > CalcJohnBMI;
-console.log(CalcMarkBMI, CalcJohnBMI);
-console.log(markHigherBMI);
+// const CalcMarkBMI = markMass / markHeight ** 2;
+// const CalcJohnBMI = johnMass / johnHeight ** 2;
+// const markHigherBMI = CalcMarkBMI > CalcJohnBMI;
+// console.log(CalcMarkBMI, CalcJohnBMI);
+// console.log(markHigherBMI);
 
-if (CalcMarkBMI > CalcJohnBMI) {
-  console.log(
-    `Mark's BMI ${CalcMarkBMI} is higher than johns BMI ${CalcJohnBMI}`
-  );
-} else {
-  console.log(`Jon's BMI ${CalcJohnBMI} is higher than Johnas${CalcMarkBMI}`);
-}
+// if (CalcMarkBMI > CalcJohnBMI) {
+//   console.log(
+//     `Mark's BMI ${CalcMarkBMI} is higher than johns BMI ${CalcJohnBMI}`
+//   );
+// } else {
+//   console.log(`Jon's BMI ${CalcJohnBMI} is higher than Johnas${CalcMarkBMI}`);
+// }
+
+const inputYear = "1991";
+console.log(inputYear);
+console.log(Number(inputYear) + 18);
+console.log(Number("khalid"));
+console.log(typeof NaN);
+console.log(String(23), 23);
