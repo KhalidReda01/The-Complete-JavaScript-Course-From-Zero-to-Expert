5435de97f198ed07a2d765d323486eba69f6ffbe
khalid Reda <khalidaziz2198@gmail.com>
2024-09-23 23:10:28 +0300

    Coding Challenge

--- js/script.js
+++ js/script.js
@@ -94,10 +94,10 @@ m tall.
 
 // let mass;
 // let height;
-const markMass = 78;
-const markHeight = 1.69;
-const johnMass = 92;
-const johnHeight = 1.95;
+// const markMass = 78;
+// const markHeight = 1.69;
+// const johnMass = 92;
+// const johnHeight = 1.95;
 
 // const age = 15;
 // const isOldEnough = age >= 18;
@@ -322,19 +322,46 @@ const age = 23;
 //   ? console.log("I like to drink adult juice")
 //   : console.log("I like to drink water");
 
-let drink2;
-if (age >= 18) {
-  drink2 = "juice";
-} else {
-  drink = "water";
-}
-console.log(drink2);
+// let drink2;
+// if (age >= 18) {
+//   drink2 = "juice";
+// } else {
+//   drink = "water";
+// }
+// console.log(drink2);
 
-console.log(`i like  to drink ${age > -28 ? "juice" : "water"}`);
-const country = "Egypt";
-const countryPopulation = 120;
-console.log(
-  `${country}'s population is ${
-    countryPopulation > 33 ? "above" : "below"
-  } average  `
-);
+// console.log(`i like  to drink ${age > -28 ? "juice" : "water"}`);
+// const country = "Egypt";
+// const countryPopulation = 120;
+// console.log(
+//   `${country}'s population is ${
+//     countryPopulation > 33 ? "above" : "below"
+//   } average  `
+// );
+
+/**
+ * Coding Challenge #4
+Let's improve Steven's tip calculator even more, this time using loops!
+Your tasks:
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate 
+tips and total values (bill + tip) for every bill value in the bills array. Use a for
+loop to perform the 10 calculations!
+Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the 
+tips and totals arrays �
+Bonus:
+4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as 
+an argument. This function calculates the average of all numbers in the given 
+array. This is a difficult challenge (we haven't done this before)! Here is how to 
+solve it:
+4.1. First, you will need to add up all values in the array. To do the addition, 
+start by creating a variable 'sum' that starts at 0. Then loop over the 
+array using a for loop. In each iteration, add the current value to the 
+'sum' variable. This way, by the end of the loop, you have all values 
+added together
+4.2. To calculate the average, divide the sum you calculated before by the 
+length of the array (because that's the number of elements)
+4.3. Call the function with the 'totals' array
+ */
