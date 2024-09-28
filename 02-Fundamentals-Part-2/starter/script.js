948b61649f9619c4c8d761c62e2af0aa6d4bb89c
khalid Reda <khalidaziz2198@gmail.com>
2024-09-28 15:57:17 +0300

    It's working you are the applystate

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -58,9 +58,28 @@ the same populations)
 1. Recreate the last assignment, but this time create an arrow function called
 'percentageOfWorld3
  */
-const percentageOfWorld3 = (populationValue) => (populationValue / 7900) * 100;
-const China1 = percentageOfWorld3(1441);
-const Egypt2 = percentageOfWorld3(120);
-const India3 = percentageOfWorld3(1300);
+const percentageOfWorld1 = (population) => (population / 7900) * 100;
+// const China1 = percentageOfWorld3(1441);
+// const Egypt2 = percentageOfWorld3(120);
+// const India3 = percentageOfWorld3(1300);
 // console.log(China, Egypt, India);
-console.log(China1, Egypt2, India3);
+// console.log(China1, Egypt2, India3);
+/**
+ * Lecture 6 Functions Calling Other Functions
+ * 
+ * . Create a function called 'describePopulation'. Use the function type you
+like the most. This function takes in two arguments: 'country' and
+'population', and returns a string like this: 'China has 1441 million people,
+which is about 18.2% of the world.'
+2. To calculate the percentage, 'describePopulation' call the
+'percentageOfWorld1' you created earlier
+3. Call 'describePopulation' with data for 3 countries of your choice
+ */
+
+function describePopulation(country, population) {
+  const percentage = percentageOfWorld1(population);
+  return `${country} has ${population} million people which is about ${percentage}of the world `;
+}
+const china = percentageOfWorld1(1441);
+console.log(describePopulation("Egypt", 120));
+console.log(china);
