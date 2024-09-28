29287222c84237717b7c484b5ebafb1605aac941
khalid Reda <khalidaziz2198@gmail.com>
2024-09-28 09:49:54 +0300

    ROMO

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,5 @@
 "use strict";
 let hasDriversLicense = false;
-const passTest = (hasDriversLicense = true);
+const passTest = true;
 if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can Drive"');
+if (hasPassed) console.log("you can drive");
