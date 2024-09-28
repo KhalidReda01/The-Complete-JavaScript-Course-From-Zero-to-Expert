4a6726755f48cf15990fb1d3e60bb631b6e2dbd2
khalid Reda <khalidaziz2198@gmail.com>
2024-09-28 10:33:09 +0300

    Lecture 3 Function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -7,9 +7,10 @@ logger();
 logger();
 logger();
 function fruitProcessor(apples, oranges) {
-  console.log(apples, oranges);
   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
   return juice;
 }
 const applesJuice = fruitProcessor(5, 0);
 console.log(applesJuice);
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
