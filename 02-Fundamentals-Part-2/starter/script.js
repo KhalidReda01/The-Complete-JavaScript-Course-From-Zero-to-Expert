4225269090b858c5be1a397ed1e3efb5f9ce6f72
khalid Reda <khalidaziz2198@gmail.com>
2024-09-28 10:40:33 +0300

    CBTD done

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,20 @@
 "use strict";
-let hasDriversLicense = false;
-const passTest = (hasDriversLicense = true);
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can Drive"');
+function logger() {
+  console.log("My name is khalid");
+}
+// calling running invoking function
+logger();
+logger();
+logger();
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with  ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const applesJuice = fruitProcessor(5, 0);
+console.log(applesJuice);
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+const num = Number("23");
+console.log(num);
