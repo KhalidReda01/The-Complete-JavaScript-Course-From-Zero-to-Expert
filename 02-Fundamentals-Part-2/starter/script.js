9d5c3b17d60e2636fdacce9d57ee6e7c62f19a7e
khalid Reda <khalidaziz2198@gmail.com>
2024-09-28 13:23:31 +0300

    Lecture 4 Function Declaration vs Function EXpressin 5m

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,16 +1,28 @@
-function logger() {
-  console.log("my name is khalid");
-}
+// function logger() {
+//   console.log("my name is khalid");
+// }
 
-// running the function invoking / calling the function
-logger();
-logger();
-logger();
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
+// // running the function invoking / calling the function
+// logger();
+// logger();
+// logger();
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+// const applesJuice = fruitProcessor(5, 0);
+// console.log(applesJuice);
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+/**
+ * lecture 4 Function Declarations vs. Expressions
+ *
+ */
+
+function calcAge1(birthYear) {
+  return 2024 - birthYear;
 }
-const applesJuice = fruitProcessor(5, 0);
-console.log(applesJuice);
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+// function declaration
+const age1 = calcAge1(1998);
+console.log(age1);
