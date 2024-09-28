d4762132682f84b46483061b227a321ab142465e
khalid Reda <khalidaziz2198@gmail.com>
2024-09-28 11:01:17 +0300

    Lecture 3 functios assingment done UIIDS

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,20 @@
 "use strict";
-let examPassed = false;
-const targetGrade = true;
-if (targetGrade) examPassed = true;
-if (examPassed) console.log(" you passed the exam");
+/**
+ * LECTURE: Functions
+1. Write a function called 'describeCountry' which takes three parameters:
+'country', 'population' and 'capitalCity'. Based on this input, the
+function returns a string with this format: 'Finland has 6 million people and its
+capital city is Helsinki'
+2. Call this function 3 times, with input data for 3 different countries. Store the
+returned values in 3 different variables, and log them to the console
+ */
+function describeCountry(country, population, capitalCity) {
+  const data = `${country} has ${population} million people and its capital city ${capitalCity}.`;
+  return data;
+}
+const firstCountry = describeCountry("Egypt", 120, "Cairo");
+const secondCountry = describeCountry("Saudi Arabi", 120, "Riad");
+const thirdCountry = describeCountry("Qatar", 120, "Doha");
+console.log(firstCountry);
+console.log(secondCountry);
+console.log(thirdCountry);
