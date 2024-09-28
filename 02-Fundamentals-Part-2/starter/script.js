408b46b41400805e1a6cbea72405b92e0351e3fd
khalid Reda <khalidaziz2198@gmail.com>
2024-09-28 13:35:56 +0300

    CBTD

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,20 +1,33 @@
-"use strict";
-function logger() {
-  console.log("My name is khalid");
-}
-// calling running invoking function
-logger();
-logger();
-logger();
-function fruitProcessor(apples, oranges) {
-  console.log(apples, oranges);
-  const juice = `Juice with  ${apples} apples and ${oranges} oranges`;
-  return juice;
-}
+// "use strict";
+// function logger() {
+//   console.log("My name is khalid");
+// }
+// // calling running invoking function
+// logger();
+// logger();
+// logger();
+// function fruitProcessor(apples, oranges) {
+//   console.log(apples, oranges);
+//   const juice = `Juice with  ${apples} apples and ${oranges} oranges`;
+//   return juice;
+// }
 
-const applesJuice = fruitProcessor(5, 0);
-console.log(applesJuice);
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
-const num = Number("23");
-console.log(num);
+// const applesJuice = fruitProcessor(5, 0);
+// console.log(applesJuice);
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+// const num = Number("23");
+// console.log(num);
+
+const age1 = calcAge1(1998);
+// function declartion
+function calcAge1(birthYear) {
+  return 2024 - birthYear;
+}
+console.log(age1);
+
+const age2 = calcAge2(1998);
+const calcAge2 = function (birthYear) {
+  return 2024 - birthYear;
+};
+console.log(age1, age2);
