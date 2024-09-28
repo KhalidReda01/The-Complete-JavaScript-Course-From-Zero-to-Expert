c66a910d75f8fdcda29a4ba1b5be12fc8df3e748
khalid Reda <khalidaziz2198@gmail.com>
2024-09-28 10:45:48 +0300

    Lecture 3 Functions ROMO done

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,18 @@
 "use strict";
-let hasDriversLicense = false;
-const passTest = true;
-if (passTest) hasDriversLicense = true;
-if (hasPassed) console.log("you can drive");
+function logger() {
+  console.log("my name is khalid");
+}
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+const appleOrangeJuice = fruitProcessor(4, 2);
+console.log(appleOrangeJuice);
