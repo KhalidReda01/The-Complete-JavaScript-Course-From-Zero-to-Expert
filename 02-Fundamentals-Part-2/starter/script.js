c9bbc58e7a8a88e635592ef83a756642144488b3
khalid Reda <khalidaziz2198@gmail.com>
2024-09-28 13:42:17 +0300

    POMO

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,18 +1,32 @@
-"use strict";
-function logger() {
-  console.log("my name is khalid");
+// "use strict";
+// function logger() {
+//   console.log("my name is khalid");
+// }
+
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+// const appleOrangeJuice = fruitProcessor(4, 2);
+// console.log(appleOrangeJuice);
+
+// function declartion vs function expression
+function calcAge1(birthYear) {
+  return 2024 - birthYear;
 }
+const age1 = calcAge1(1998);
+console.log(age1);
 
-logger();
-logger();
-logger();
-
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
-  return juice;
-}
-
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-const appleOrangeJuice = fruitProcessor(4, 2);
-console.log(appleOrangeJuice);
+// function expression
+const calcAge2 = function (birthYear) {
+  return 2024 - birthYear;
+};
+const age2 = calcAge2(1998);
+console.log(age1, age2);
