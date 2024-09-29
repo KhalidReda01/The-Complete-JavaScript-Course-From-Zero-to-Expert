55345c5e17c7bb92f096ceebf709c00927cac868
khalid Reda <khalidaziz2198@gmail.com>
2024-09-29 21:21:37 +0300

    Solve the assignment

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -76,14 +76,14 @@ const percentageOfWorld1 = (population) => (population / 7900) * 100;
 // 3. Call 'describePopulation' with data for 3 countries of your choice
 //  */
 
-function describePopulation(country, population) {
-  const percentage = percentageOfWorld1(population);
-  return `${country} has ${population} million people which is about ${percentage}of the world `;
-}
-// const china = percentageOfWorld1(1441);
-console.log(describePopulation("Egypt", 120));
-console.log(describePopulation("china", 1440));
-console.log(describePopulation("saudi", 40));
+// function describePopulation(country, population) {
+//   const percentage = percentageOfWorld1(population);
+//   return `${country} has ${population} million people which is about ${percentage}of the world `;
+// }
+// // const china = percentageOfWorld1(1441);
+// console.log(describePopulation("Egypt", 120));
+// console.log(describePopulation("china", 1440));
+// console.log(describePopulation("saudi", 40));
 // console.log(china);
 /**
  * UIIDs
@@ -100,12 +100,42 @@ percentage values
 
 // };
 
-const populationValue = [120, 1330, 10, 30];
-console.log(populationValue.length >= 4);
-const percentages = [
-  percentageOfWorld1(populationValue[0]),
-  percentageOfWorld1(populationValue[1]),
-  percentageOfWorld1(populationValue[2]),
-  percentageOfWorld1(populationValue[3]),
-];
-console.log(percentages);
+// const populationValue = [120, 1330, 10, 30];
+// console.log(populationValue.length === 4);
+// const percentages = [
+//   percentageOfWorld1(populationValue[0]),
+//   percentageOfWorld1(populationValue[1]),
+//   percentageOfWorld1(populationValue[2]),
+//   percentageOfWorld1(populationValue[3]),
+// ];
+// console.log(percentages);
+
+/** LECTURE: Basic Array Operations (Methods)
+1. Create an array containing all the neighbouring countries of a country of your
+choice. Choose a country which has at least 2 or 3 neighbours. Store the array
+into a variable called 'neighbours'
+2. At some point, a new country called 'Utopia' is created in the neighbourhood of
+your selected country. So add it to the end of the 'neighbours' array
+3. Unfortunately, after some time, the new country is dissolved. So remove it from
+the end of the array
+4. If the 'neighbours' array does not include the country ‘Germany’, log to the
+console: 'Probably not a central European country :D'
+5. Change the name of one of your neighbouring countries. To do that, find the
+index of the country in the 'neighbours' array, and then use that index to
+change the array at that index position. For example, you can search for
+'Sweden' in the array, and then replace it with 'Republic of Sweden'. */
+const neighbours = ["Sudan", "Libya", "Palestine", "saudi", "Turkey"];
+console.log(neighbours);
+neighbours.push("Utopia");
+console.log(neighbours);
+neighbours.pop();
+console.log(neighbours);
+if (!neighbours.includes("Germany")) {
+  console.log("Probably not a central Europian Country");
+}
+
+neighbours.indexOf("Sudan");
+console.log(neighbours.indexOf("Sudan"));
+// neighbours[0] = "Lebneon";
+neighbours[neighbours.indexOf("Sudan")] = "Lebneon";
+console.log(neighbours);
