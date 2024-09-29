bf4ec4b23ba3707d0c811fb79b20b9af2f7eca90
khalid Reda <khalidaziz2198@gmail.com>
2024-09-29 15:18:12 +0300

    Start the Challenge 1 at Function Practice

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -73,21 +73,75 @@
 /**
  * Lecture 7 Reviewing Functions
  */
-const calcAge = function (birthYear) {
-  return 2024 - birthYear;
-};
-const yearsUntilRetirement = function (birthYear, firstName) {
-  const age = calcAge(birthYear);
-  const retirement = 65 - age;
-  if (retirement > 0) {
-    console.log(`${firstName} retires in ${retirement} years`);
-    return retirement;
-  } else {
-    return -1;
-    console.log(`${firstName} retires in ${retirement} years`);
-  }
-  // return retirement;
-  // return `${firstName} retires in ${retirement} years`;
-};
-console.log(yearsUntilRetirement(1998, "khalid"));
-console.log(yearsUntilRetirement(1970, "Youssef"));
+// const calcAge = function (birthYear) {
+//   return 2024 - birthYear;
+// };
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//   const age = calcAge(birthYear);
+//   const retirement = 65 - age;
+//   if (retirement > 0) {
+//     console.log(`${firstName} retires in ${retirement} years`);
+//     return retirement;
+//   } else {
+//     return -1;
+//     console.log(`${firstName} retires in ${retirement} years`);
+//   }
+//   // return retirement;
+//   // return `${firstName} retires in ${retirement} years`;
+// };
+// console.log(yearsUntilRetirement(1998, "khalid"));
+// console.log(yearsUntilRetirement(1970, "Youssef"));
+
+/**
+ * Lecture 8 Coding Challenge 1
+ *
+ */
+
+/**
+ * / const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas) {
+//   console.log('Dolphins win the trophy 🏆');
+// } else if (scoreKoalas > scoreDolphins) {
+  //   console.log('Koalas win the trophy 🏆');
+  // } else if (scoreDolphins === scoreKoalas) {
+//   console.log('Both win the trophy!');
+// }
+
+// BONUS 1
+const scoreDolphins = (97 + 112 + 80) / 3;
+const scoreKoalas = (109 + 95 + 50) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log('Dolphins win the trophy 🏆');
+  } else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+    console.log('Koalas win the trophy 🏆');
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+  console.log('Both win the trophy!');
+} else {
+  console.log('No one wins the trophy 😭');
+}
+// Coding Challenge #1
+// Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
+// gymnastics discipline, which works differently.
+// Each team competes 3 times, and then the average of the 3 scores is calculated (so
+// one average score per team).
+// A team only wins if it has at least double the average score of the other team.
+// Otherwise, no team wins!
+// Your tasks:
+// 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+// 2. Use the function to calculate the average for both teams
+// 3. Create a function 'checkWinner' that takes the average score of each team
+// as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
+// to the console, together with the victory points, according to the rule above.
+// Example: "Koalas win (30 vs. 13)"
+// 4. Use the 'checkWinner' function to determine the winner for both Data 1 and
+// Data 2
+// 5. Ignore draws this time
+// Test data:
+// § Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+// § Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+ */
