ca1aac175ad1f0cfbc85d194fc5cd460d24aaf78
khalid Reda <khalidaziz2198@gmail.com>
2024-09-29 21:02:59 +0300

    UIIDS assignment start for the last 2 lecture

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -222,27 +222,27 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
 //   calcAge(years2[years2.length - 1]),
 // ];
 // console.log(ages2);
-// add element
-const friends = ["khalid", "reda", "ahmed", "abdo"];
-// at the end
-const friendspushed = friends.push("abdelaziz");
-console.log(friendspushed);
-console.log(friends);
-friends.unshift("hoda");
-console.log(friends);
-// Remove elemnts
-friends.pop(); // last
-const popped = friends.pop(); // last
-console.log(popped);
-console.log(friends);
-friends.shift(); // first
-console.log(friends);
-friends.push(23);
+// // add element
+// const friends = ["khalid", "reda", "ahmed", "abdo"];
+// // at the end
+// const friendspushed = friends.push("abdelaziz");
+// console.log(friendspushed);
+// console.log(friends);
+// friends.unshift("hoda");
+// console.log(friends);
+// // Remove elemnts
+// friends.pop(); // last
+// const popped = friends.pop(); // last
+// console.log(popped);
+// console.log(friends);
+// friends.shift(); // first
+// console.log(friends);
+// friends.push(23);
 
-console.log(friends.indexOf("pop"));
-console.log(friends.includes("peter"));
-console.log(friends.includes("khalid"));
-console.log(friends.includes(23));
-if (friends.includes("ahmed")) {
-  console.log("You  have a friend called Ahmed");
-}
+// console.log(friends.indexOf("pop"));
+// console.log(friends.includes("peter"));
+// console.log(friends.includes("khalid"));
+// console.log(friends.includes(23));
+// if (friends.includes("ahmed")) {
+//   console.log("You  have a friend called Ahmed");
+// }
