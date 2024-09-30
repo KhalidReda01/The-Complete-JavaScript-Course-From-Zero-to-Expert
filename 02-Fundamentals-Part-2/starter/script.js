0ddc0bfb58e8cf943a28c815f390d51b28b2d690
khalid Reda <khalidaziz2198@gmail.com>
2024-09-30 13:43:37 +0300

    There is a bug

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -247,9 +247,10 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
 //   console.log("You  have a friend called Ahmed");
 // }
 /**
- * Lecture 11 Coding Challenge 2 
- * Coding Challenge #2
-Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+ 
+ Lecture 11 Coding Challenge 2 
+Coding Challenge #2
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%. Done
 Your tasks:
 1.  Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100
 2.  And now let's use arrays! So create an array 'bills' containing the test data below
@@ -257,3 +258,15 @@ Your tasks:
 4.  Bonus: Create an array 'total' containing the total values, so the bill + tip
 Test data: 125, 555 and 44
  */
+// const bill = 301;
+let tip;
+
+console.log(tip);
+function calTip(bill) {
+  if (bill >= 50 && bill <= 300) {
+    return (tip = 0.15 * bill);
+  } else {
+    return (tip = 0.2 * bill);
+  }
+}
+calTip(350);
