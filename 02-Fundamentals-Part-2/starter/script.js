b76a9d78379ab298595f2c0b69486d6035db092e
khalid Reda <khalidaziz2198@gmail.com>
2024-10-01 11:32:20 +0300

    Assignment done Lecture Object Methods

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -151,19 +151,36 @@ const myCountry = {
   language: "Arabic",
   population: 120,
   neighbours: ["sudan", "libya", "saudi", "palestine", "Turkey", "Lebnon"],
+  describe: function () {
+    return `${this.country} has ${this.population} million ${this.language}-speaking people,${this.neighbours.length} neigbhouring countries and a capital called ${this.capital}`;
+  },
+  checkIsLand: function () {
+    return this.neighbours ? (this.isIland = true) : (this.isIland = false);
+  },
 };
+console.log(myCountry.describe());
+console.log(myCountry.checkIsLand());
+console.log(myCountry.isIland);
 /**
  * Lecture Dot vs Bracket Notation
  *Using the object from the previous assignment, log a string like this to the console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries and a capital called Helsinki.'
 2.  Increase the country's population by two million using dot notation, and then decrease it by two million using brackets notation.
  */
-console.log(
-  `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people,${myCountry.neighbours.length} neigbhouring countries and a capital called ${myCountry.capital}`
-);
-myCountry.population = myCountry.population + 2;
-console.log(myCountry);
-myCountry.population = myCountry.population - 2;
-console.log(myCountry);
-console.log(
-  `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people,${myCountry.neighbours.length} neigbhouring countries and a capital called ${myCountry.capital}`
-);
+// console.log(
+//   `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people,${myCountry.neighbours.length} neigbhouring countries and a capital called ${myCountry.capital}`
+// );
+// myCountry.population = myCountry.population + 2;
+// console.log(myCountry);
+// myCountry.population = myCountry.population - 2;
+// console.log(myCountry);
+// console.log(
+//   `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people,${myCountry.neighbours.length} neigbhouring countries and a capital called ${myCountry.capital}`
+// );
+
+/**
+ * LECTURE: Object Methods
+1.  Add a method called 'describe' to the 'myCountry' object. This method will log a string to the console, similar to the string logged in the previous assignment, but this time using the 'this' keyword.
+2.  Call the 'describe' method
+3.  Add a method called 'checkIsland' to the 'myCountry' object. This method will set a new property on the object, called 'isIsland'. 
+'isIsland' will be true if there are no neighbouring countries, and false if there are. Use the ternary operator to set the property
+ */
