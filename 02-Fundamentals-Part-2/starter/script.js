1e89e364528849891e4bf0472ec9590a1d722c77
khalid Reda <khalidaziz2198@gmail.com>
2024-10-01 16:54:07 +0300

    Lecture done

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -470,3 +470,10 @@ const khalid = [
 for (let i = khalid.length - 1; i >= 0; i--) {
   console.log(i, khalid[i]);
 }
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`-----starting excercise${exercise}`);
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Lifting weight repetion ${rep}`);
+  }
+}
