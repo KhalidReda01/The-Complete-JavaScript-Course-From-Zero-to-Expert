999356e0c31de23f72ed8f4bcccffa752fba3c32
khalid Reda <khalidaziz2198@gmail.com>
2024-10-02 21:49:57 +0300

    What is happeing at git

---
+++ 05-Guess-My-Number/starter/Lecture 8 Manipulating css.md
@@ -0,0 +1,9 @@
+# Lecture 8 Manipulating css
+
+```js
+ document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+```
+
+
+
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,16 +1,23 @@
 'use strict';
 // console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = 'Correct Number';
+// document.querySelector('.message').textContent = ' Correct Number';
 // console.log(document.querySelector('.message').textContent);
+
 // document.querySelector('.number').textContent = 13;
 // document.querySelector('.score').textContent = 10;
-// document.querySelector('.guess').value = 23;
 
+// document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   if (!guess) {
-    document.querySelector('.message').textContent = 'No Number';
+    // when there  is no input
+    document.querySelector('.message').textContent = ' NO number';
+    // when player wins
   }
 });
