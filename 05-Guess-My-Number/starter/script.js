bd4ac29bc8408d6bf2decc125f5d77fcf8d81bb5
khalid Reda <khalidaziz2198@gmail.com>
2024-10-02 18:42:36 +0300

    Zero lost game bug

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,6 +10,7 @@
 // console.log(document.querySelector('.guess').value);
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -19,8 +20,16 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = ' Correct Number';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too high!📈';
+    if (score > 1) {
+      score--;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.message').textContent = 'Too high!📈';
+    } else {
+      document.querySelector('.message').textContent = 'YOu lost the game';
+    }
   } else if (guess < secretNumber) {
     document.querySelector('.message').textContent = 'Too low!📉';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
