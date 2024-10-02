88f6dae6293b46353f3bfb5486553e5fa09248b2
khalid Reda <khalidaziz2198@gmail.com>
2024-10-02 15:54:22 +0300

    ROMO

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,6 @@
 'use strict';
 console.log(document.querySelector('.message').textContent);
-console.log('romo');
+console.log(document.querySelector('.number').textContent);
+document.querySelector('.number').textContent = 23;
+document.querySelector('.guess').value = 23;
+document.querySelector('.score').textContent = 23;
