ae3fecf133903f986ac207a84761abb90383f9b4
khalid Reda <khalidaziz2198@gmail.com>
2024-10-02 18:23:23 +0300

    8m

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -9,12 +9,18 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   if (!guess) {
     document.querySelector('.message').textContent = ' NO number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = ' Correct Number';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high!📈';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!📉';
   }
 });
