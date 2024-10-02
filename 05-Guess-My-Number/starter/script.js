86a323397bd868e55623172d7ccf656c678e7f90
khalid Reda <khalidaziz2198@gmail.com>
2024-10-02 17:10:01 +0300

    ROMO this is why it's so impartant really

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,6 +1,14 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
-console.log(document.querySelector('.number').textContent);
-document.querySelector('.number').textContent = 23;
-document.querySelector('.guess').value = 23;
-document.querySelector('.score').textContent = 23;
+// console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.number').textContent);
+// document.querySelector('.number').textContent = 23;
+// document.querySelector('.guess').value = 23;
+// document.querySelector('.score').textContent = 23;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number';
+  }
+});
