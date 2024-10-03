5730d6ddb197d3101c985573dd57876e82280028
khalid Reda <khalidaziz2198@gmail.com>
2024-10-03 17:18:27 +0300

    7m

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -36,33 +36,35 @@ document.querySelector('.check').addEventListener('click', function () {
     if (score > 1) {
       score--;
       document.querySelector('.score').textContent = score;
-      document.querySelector('.message').textContent = 'Too high!📈';
-    } else {
-      document.querySelector('.message').textContent = 'YOu lost the game';
-      document.querySelector('.score').textContent = 0;
-    }
-    // when guess  is too high
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      score--;
-      document.querySelector('.score').textContent = score;
-      document.querySelector('.message').textContent = 'Too high!📈';
-    } else {
-      document.querySelector('.message').textContent = 'YOu lost the game';
-      document.querySelector('.score').textContent = 0;
-    }
-    // when guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      score--;
-      document.querySelector('.message').textContent = 'Too low!📈';
-      document.querySelector('.score').textContent = score;
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'Too high!📈' : 'Too low!📈';
     } else {
       document.querySelector('.message').textContent = 'YOu lost the game';
       document.querySelector('.score').textContent = 0;
     }
   }
 });
+
+// when guess  is too high
+// } else if (guess > secretNumber) {
+//   if (score > 1) {
+//     score--;
+//     document.querySelector('.score').textContent = score;
+//     document.querySelector('.message').textContent = 'Too high!📈';
+//   } else {
+//     document.querySelector('.message').textContent = 'YOu lost the game';
+//     document.querySelector('.score').textContent = 0;
+//   }
+//   // when guess is too low
+// } else if (guess < secretNumber) {
+//   if (score > 1) {
+//     score--;
+//     document.querySelector('.message').textContent = 'Too low!📈';
+//     document.querySelector('.score').textContent = score;
+//   } else {
+//     document.querySelector('.message').textContent = 'YOu lost the game';
+//     document.querySelector('.score').textContent = 0;
+//   }
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
