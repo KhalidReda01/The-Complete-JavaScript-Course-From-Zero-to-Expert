8bb98a1326c7e371608e8cc91239aee9862f9f4f
khalid Reda <khalidaziz2198@gmail.com>
2024-10-03 15:34:16 +0300

    A bug

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,7 +11,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-let highscore = 0;
+let highScore = 0;
 document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -25,8 +25,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
-    if (score > highscore) highscore = score;
-    document.querySelector('.score').textContent = highscore;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.score').textContent = highScore;
+    }
+
     // when gess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
