5cc2239c2e60774084f636385b95292c08eb692f
khalid Reda <khalidaziz2198@gmail.com>
2024-10-03 15:40:46 +0300

    Sove the bug

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,7 +12,7 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
-document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -22,12 +22,13 @@ document.querySelector('.check').addEventListener('click', function () {
     // when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = ' Correct Number';
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
     if (score > highScore) {
       highScore = score;
-      document.querySelector('.score').textContent = highScore;
+      document.querySelector('.highscore').textContent = highScore;
     }
 
     // when gess is too high
