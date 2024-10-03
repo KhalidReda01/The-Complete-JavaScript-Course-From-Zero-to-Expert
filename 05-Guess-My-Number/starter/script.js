21c7341830aa472621285a3145cea52be20437a4
khalid Reda <khalidaziz2198@gmail.com>
2024-10-03 15:56:17 +0300

    CBTD ROMO Done

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -9,9 +9,10 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let highScore = 0;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -21,10 +22,16 @@ document.querySelector('.check').addEventListener('click', function () {
     // when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number';
+    document.querySelector('.number').textContent = secretNumber;
+    console.log(secretNumber);
     document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('number').style.width = '30rem';
+    document.querySelector('.number').style.width = '30rem';
 
     document.querySelector('.score').textContent = score;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
     // when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -44,4 +51,13 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'you lost the game';
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'start guessing...';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
