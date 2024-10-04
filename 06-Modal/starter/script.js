26994832054313ad74b2cfdbeeed6204587d3989
khalid Reda <khalidaziz2198@gmail.com>
2024-10-04 14:58:07 +0300

    Lecture 14 Project Done CBTD

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -3,17 +3,24 @@ const modal = document.querySelector('.modal');
 const overLay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('button clicked');
-    modal.classList.remove('hidden');
-    overLay.classList.remove('hidden');
-  });
-}
 
+const openModal = function () {
+  console.log('button clicked');
+  modal.classList.remove('hidden');
+  overLay.classList.remove('hidden');
+};
 const closeModal = function () {
   modal.classList.add('hidden');
   overLay.classList.add('hidden');
 };
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
 btnCloseModal.addEventListener('click', closeModal);
 overLay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
+});
