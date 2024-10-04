307caa720b6e380055f6745ae8a45789c1ea9396
khalid Reda <khalidaziz2198@gmail.com>
2024-10-04 14:52:55 +0300

    Lecture 13 Working with Classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,8 +1,19 @@
 'use strict';
-const modal = document.querySelector('modal');
+const modal = document.querySelector('.modal');
 const overLay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('button clicked');
+    modal.classList.remove('hidden');
+    overLay.classList.remove('hidden');
+  });
 }
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overLay.classList.add('hidden');
+};
+btnCloseModal.addEventListener('click', closeModal);
+overLay.addEventListener('click', closeModal);
