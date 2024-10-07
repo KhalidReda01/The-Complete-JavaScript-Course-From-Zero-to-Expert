b2efb2e6be75c31c0e33b98e27fb57d6ee71544d
khalid Reda <khalidaziz2198@gmail.com>
2024-10-07 15:36:47 +0300

    Lecture 17 Switching the active Player 5m

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -12,7 +12,9 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
@@ -25,6 +27,8 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
     current0El.textContent = currentScore; // Change Later
   } else {
     // switch to next player
